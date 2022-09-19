bc43afe0abdb2a692d2addd1b09a14bf87f423e8
Cadi7 <finicacr7@gmail.com>
2022-09-19 16:46:43 +0300

    Ascuns secret_key

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6mfu__uq3m%0kwsw15oum99*xmyoyzi_qg97i+7n87p4sbc36k'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
